b443e4f7b58fd6e549011d954d192ee7fc1b6143
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-09-12 11:24:32 -0700

    Add filter for credit transactions

--- src/options/Table.tsx
+++ src/options/Table.tsx
@@ -1,28 +1,30 @@
 import * as React from 'react';
-import { DataGrid, GridColDef, GridRowId, GridValueGetterParams } from '@material-ui/data-grid';
+import { DataGrid, GridColDef} from '@material-ui/data-grid';
 import Transaction from '../models/Transaction';
 import { useState } from 'react';
-import Grid from '@material-ui/core/Grid'
+import Grid from '@material-ui/core/Grid';
+import "./Table.scss";
+import Check from '@material-ui/icons/Check';
+import Clear from '@material-ui/icons/Clear';
 
 const simpleColumns: GridColDef[] = [
     {
         field: 'date',
         headerName: 'Date',
-        width: 75,
+        width: 150,
     },
     {
         field: 'originalDescription',
         headerName: 'Description',
-        width: 175,
+        flex: 175
     },
     {
         field: 'amount',
         headerName: 'Amount',
         type: 'number',
-        width: 100,
+        width: 150,
     }
 ]
-
 
 const columns: GridColDef[] = [
     ...simpleColumns,
@@ -31,13 +33,7 @@ const columns: GridColDef[] = [
         headerName: 'Account',
         type: 'string',
         width: 200,
-    },
-    {
-        field: 'transactionType',
-        headerName: 'Type',
-        type: 'string',
-        width: 200,
-    },
+    }
 
 ];
 
@@ -45,48 +41,123 @@ type TableProps = {
     rows: Transaction[]
 }
 
+type Filters = {
+    credits: boolean
+}
+
 export default function DataGridDemo({ rows }: TableProps) {
 
     const [selectedRows, setSelectedRows] = useState<Transaction[]>([]);
+    const [filters, setFilters] = useState<Filters>({ credits: true });
 
     const getTotal = (): number => {
-        let total : number = 0.0;
+        let total: number = 0.0;
         selectedRows.forEach(selectedTransaction => {
-            total+= selectedTransaction.amount;
+            total += selectedTransaction.amount;
         })
         return total;
     }
+
+    const copyTable = () => {
+        const elTable = document.querySelector('#output-table');
+
+        let range, sel;
+
+        // Ensure that range and selection are supported by the browsers
+        if (document.createRange && window.getSelection) {
+
+            range = document.createRange();
+            sel = window.getSelection();
+            // unselect any element in the page
+            if (sel && elTable) {
+                sel.removeAllRanges();
+
+                try {
+                    range.selectNodeContents(elTable);
+                    sel.addRange(range);
+                } catch (e) {
+                    range.selectNode(elTable);
+                    sel.addRange(range);
+                }
+
+                document.execCommand('copy');
+
+
+                sel.removeAllRanges();
+            }
+        }
+    }
+
+    const filteredRows = rows.filter(transaction => {
+        if (filters.credits) {
+            return transaction.transactionType !== "credit"
+        } else {
+            return true;
+        }
+    })
 
 
     getTotal();
 
     return (
         <>
-        <Grid container>
-          
-        <div style={{ height: 800, width: '50%' }}>
-            Shared Transaction Total: {getTotal()}
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                checkboxSelection
-                onSelectionModelChange={(newSelectionModel) => {
-                    setSelectedRows(newSelectionModel.map(rowId => {
-                        return rows[rowId as number]
-                    }));
-                }}
-            />
+            <Grid container>
+                <Grid className="filters-container" container>
+                    <p>Filters: </p>
+                    <button onClick={()=>{setFilters({credits: !filters.credits})}}>
+                        {filters.credits ? <Check /> : <Clear /> }
+                        Credit Transactions</button>
+                </Grid>
+                <div style={{ height: 800, width: '50%' }}>
+                    <DataGrid
+                        rows={filteredRows}
+                        columns={columns}
+                        checkboxSelection
+                        onRowClick={(stuff, event) => {
+                            //Prevents need for double click to select
+                            event.preventDefault();
+                        }}
+                        onSelectionModelChange={(newSelectionModel) => {
+                            setSelectedRows(newSelectionModel.map(rowId => {
+                                return rows[rowId as number]
+                            }));
+                        }}
+                    />
 
-        </div>
-        <div style={{ height: 800, width: '40%', marginLeft: "2em" }}>
-            Output
-            <DataGrid
+                </div>
+                <div style={{ height: 800, width: '40%', marginLeft: "2em" }}>
+                    Output
+                    {/* <DataGrid
                 rows={selectedRows}
                 columns={simpleColumns}
-            />
-
-        </div>
-        </Grid>
+            /> */}
+                    <table id="output-table">
+                        <tr>
+                            <th>Name</th>
+                            <th>Amount</th>
+                        </tr>
+                        {selectedRows.map(each => {
+                            return <tr>
+                                <td>{each.originalDescription}</td>
+                                <td>{each.amount}</td>
+                            </tr>
+                        })}
+                        <tr>
+                            <th>Totals</th>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>Full: {getTotal().toFixed(2)}</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>Split: {(getTotal() / 2).toFixed(2)}</td>
+                        </tr>
+                    </table>
+                </div>
+                <button onClick={copyTable}>Copy table</button>
+            </Grid>
         </>
     );
 }
