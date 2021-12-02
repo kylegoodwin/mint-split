744ba1a6cd2b8ae72efbd9b0cafc643603571762
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2021-12-02 08:24:05 -0500

    Refactor transaction download code into its own file

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -1,8 +1,4 @@
-import Papa from 'papaparse';
-import Transaction from './types/Transaction';
-import {DateTime} from 'luxon';
-import MessageType from './types/MessageType';
-import RawTable from './types/RawTable';
+import executeDownload from './utility/download-transactions'
 
 // Listen to messages sent from other parts of the extension.
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
@@ -16,29 +12,3 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
     return isResponseAsync;
 });
-
-function executeDownload(): void {
-
-    chrome.runtime.sendMessage({type: MessageType.TransactionsLoading})
-
-    Papa.parse<RawTable>("https://mint.intuit.com/transactionDownload.event?queryNew=&offset=0&filterType=cash&comparableType=8", {
-        download: true,
-        header: true,
-        transformHeader:fixKey,
-        complete: function (results) {
-           
-            const transactions: Transaction[] = results.data.filter(value => {
-                return (DateTime.fromFormat(value.date ,"M/dd/yyyy").startOf('day').diffNow('days').days * -1) < 60;
-            }).map( rawTransaction => { 
-                return {...rawTransaction, amount: parseFloat(rawTransaction.amount)* (rawTransaction.transactionType === "debit" ? 1 : -1)} as Transaction
-            })
-
-            chrome.storage.local.set({transactionTable: transactions});
-            chrome.runtime.sendMessage({type: MessageType.TransactionsLoaded})
-        }
-    });
-}
-
-function fixKey(str: string): string {
-    return (str.charAt(0).toLowerCase() + str.slice(1)).replace(/ /g,'');
-}
